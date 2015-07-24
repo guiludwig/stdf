57d1010f544ca4d685662bf4410199650c9884e5
guiludwig <guilhermeludwig@gmail.com>
2015-07-24 15:42:54 -0500

    loadings added

--- R/printPred.R
+++ R/printPred.R
@@ -1,18 +1,20 @@
 print.stdfPred <- function(x, digits = max(3L, getOption("digits") - 3L), ...) {
-  cat("\nSpatio-Temporal Data Fusion object\n\n", sep = "")
   if (length(x$fitted)) {
     cat("Predicted values:\n")
     print.default(format(x$fitted, digits = digits), print.gap = 2L, 
                   quote = FALSE)
+    cat("\n")
   }
-  else cat("\tNo values predicted\n")
-  cat("\n")
   if (length(x$MSPEKrig)) {
     cat("Mean Squared Prediction Error:\n")
         print.default(format(x$MSPEKrig, digits = digits), print.gap = 2L, 
                       quote = FALSE)
+  } else if(length(x$loadings)){
+    cat("Loadings:\n")
+    print(x$loadings)
+  } else {
+    cat("\tI could not calculate MSPE\n")
   }
-  else cat("\tI could not calculate MSPE\n")
   cat("\n")
   invisible(x)
 }
